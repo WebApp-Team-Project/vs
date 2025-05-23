f819be40d885127c2bf30022f19f39bfa1b80aa9
OneshotTwokill <one1024@tukorea.ac.kr>
2025-05-23 14:49:56 +0900

    first commit ^-^

---
+++ src/components/Comment.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { useState, useEffect } from 'react';
+import '../index.css'
+
+function Comment(props){
+    const {userId, content, date, likes, voteOptionId} = props;
+
+    return(
+        <Comment>
+            <div>
+                <h1>&lt;{userId || "익명" }&lt;</h1>
+                <div>
+                    <img src="따봉아이콘" alt="따봉아이콘" />
+                    <p>{likes || " "}</p>
+                </div> 
+            </div>
+            <p>{content || "댓글 내용입니다." }</p>
+            <p>{date || "timestamp" }</p>
+        </Comment>
+    )
+}
+
+export default Comment;
