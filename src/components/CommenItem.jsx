b74db704bfda8a0dba696fe4cb8ea0f71de0c019
OneshotTwokill <one1024@tukorea.ac.kr>
2025-05-23 16:00:47 +0900

    styling

--- src/components/CommenItem.jsx
+++ src/components/CommenItem.jsx
@@ -2,25 +2,66 @@ import styled from "styled-components";
 import { useState, useEffect } from 'react';
 import '../index.css'
 
-const CommentItem = styled.div`
-background-color: red;
+const CommentItemContainer = styled.div`
+width: 393px;
+padding: 12px 30px;
+
+border-bottom: 0.5px solid #505050;
+`
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+gap:3px;
+
+div{
+display:flex;
+justify-content: space-between;
+align-items: center;
+align-self: stretch;
+}
+`
+
+const UserName = styled.h1`
+font-size: 11px; font-weight: bold;
+`
+
+const IconBox = styled.div`
+display: flex;
+gap:2px;
+
+p{
+font-size:10px; font-weight:bold;
+}
+`
+
+const Content = styled.p`
+font-size:11px;
+`
+
+const TimeStamp = styled.p`
+font-size:6px;
 `
 
 function CommentItem(props){
-    const {userId, content, date, likes, voteOptionId} = props;
+    const {userId, content, timestamp , likes, voteOptionId} = props;
 
     return(
-        <CommentItem>
-            <div>
-                <h1>&lt;{userId || "익명" }&lt;</h1>
+        <CommentItemContainer>
+            <Container>
                 <div>
-                    <img src="따봉아이콘" alt="따봉아이콘" />
-                    <p>{likes || " "}</p>
-                </div> 
-            </div>
-            <p>{content || "댓글 내용입니다." }</p>
-            <p>{date || "timestamp" }</p>
-        </CommentItem>
+                    <UserName>&lt;{userId || "익명1" }&gt;</UserName>
+                    <IconBox>
+                        <img src="따봉아이콘"/>
+                        <p>{likes || "0"}</p>
+                    </IconBox> 
+                </div>
+                <Content>{content || "댓글 내용입니다." }</Content>
+                <div>
+                <TimeStamp>{timestamp || "5/23 17:54" }</TimeStamp>
+                </div>
+            </Container>
+        </CommentItemContainer>
     )
 }
 
