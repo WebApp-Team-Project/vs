9c68e9ec3af9f47a4b1f855be64ac0d7d24474f3
OneshotTwokill <one1024@tukorea.ac.kr>
2025-05-23 16:10:20 +0900

    CommentItemList.jsx

---
+++ src/components/CommentItemList.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { useState, useEffect } from 'react';
+import '../index.css';
+import CommentItem from "./CommenItem";
+
+function CommentItemlist(props){
+
+    return{
+
+    }
+}
+
+export default CommentItemlist
