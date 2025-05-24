9e5f2519d62dd5c8a0e17a89af2c257a6e673fb5
nagosu <nozick1021@gmail.com>
2025-05-24 18:58:01 +0900

    write 페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import MainPage from './pages/MainPage';
+import PostWritePage from './pages/PostWritePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainPage />} />
+        <Route path='/write' element={<PostWritePage />} />
       </Routes>
     </BrowserRouter>
   );
