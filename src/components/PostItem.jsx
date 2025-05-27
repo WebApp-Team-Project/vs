bafa4eeb1a91544d915691c425b669c70b11ecc9
OneshotTwokill <one1024@tukorea.ac.kr>
2025-05-27 10:02:14 +0900

    postitem

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,3 +1,20 @@
 import styled from "styled-components";
 import { useState, useEffect } from 'react';
 import '../index.css';
+
+const Container = styled.div`
+background-color
+`
+
+function PostItem(props){
+
+    const { category, title, content, likes, timelimit, comments } =props;
+
+    return(
+        <Container>
+            PostItem
+        </Container>
+    )
+}
+
+export default PostItem;
