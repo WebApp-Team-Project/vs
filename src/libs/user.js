e6c99f3d15a256ffe032c06211abb395175f837c
nagosu <nozick1021@gmail.com>
2025-05-30 15:29:37 +0900

    로컬스토리지 UID 필드명 변경

--- src/libs/user.js
+++ src/libs/user.js
@@ -1,18 +1,17 @@
 // localStorage에 uid 저장
 export const setUidToLocalStorage = uid => {
-  const existingUid = localStorage.getItem('uid');
+  const existingUid = localStorage.getItem('WEBAPP_TEAM_PROJECT_UID');
   if (!existingUid) {
-    localStorage.setItem('uid', uid);
-    console.log(`uid 저장: ${uid}`);
+    localStorage.setItem('WEBAPP_TEAM_PROJECT_UID', uid);
   }
 };
 
 // localStorage에서 uid 가져오기
 export const getUidFromLocalStorage = () => {
-  return localStorage.getItem('uid');
+  return localStorage.getItem('WEBAPP_TEAM_PROJECT_UID');
 };
 
 // localStorage에서 uid 삭제
 export const removeUidFromLocalStorage = () => {
-  localStorage.removeItem('uid');
+  localStorage.removeItem('WEBAPP_TEAM_PROJECT_UID');
 };
